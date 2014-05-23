3ef2380571253c7d3db932660338f2ee9c206a82
Kristin Peterson <kristinpeterson@me.com>
2014-05-23 13:15:13 -0700

    fix typo

--- euler_4.py
+++ euler_4.py
@@ -1,5 +1,5 @@
 """
-Largest Palindrome Project
+Largest Palindrome Product
 
 A palindromic number reads the same both ways.
 The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
